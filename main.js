4ae31f600e536c1b3e4131316bb6d10ac50d9390
Lou Sempere <lousempere@gmail.com>
2023-05-27 15:46:49 +0200

    ca marche

---
+++ main.js
@@ -0,0 +1,255 @@
+let toggle = document.querySelector("#header .toggle-button");
+let collapse = document.querySelectorAll("#header .collapse");
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".close-btn");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+// document.getElementById("login") => div id="login"
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", (event) => {
+  event.preventDefault();
+  // event.stopPropagation();
+  wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+  wrapper.classList.remove("active");
+});
+
+toggle.addEventListener("click", function () {
+  collapse.forEach((col) => col.classList.toggle("collapse-toggle"));
+});
+
+// with masonry
+// new Masonry("#posts .grid", {
+//     itemSelector : '.grid-item',
+//     gutter : 20
+// });
+
+// swiper libray initialization
+new Swiper(".swiper-container", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 5,
+  autoplay: {
+    delay: 3000,
+  },
+  // responsive brakpoints
+  breakpoints: {
+    "@0": {
+      slidesPerView: 2,
+    },
+    // 888px
+    "@1.00": {
+      slidesPerView: 3,
+    },
+    // 1110px
+    "@1.25": {
+      slidesPerView: 4,
+    },
+    // 1330px
+    "@1.50": {
+      slidesPerView: 5,
+    },
+  },
+});
+
+// Sticky Navigation
+window.onscroll = function () {
+  myFunction();
+};
+
+// get the current value
+let navbar = document.getElementById("header");
+
+// get the navbar position
+let sticky = navbar.offsetTop;
+
+// sticky function
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Envoyer les données du formulaire de login au serveur
+document.addEventListener("DOMContentLoaded", () => {
+  const formLogin = document.querySelector("#loginForm");
+  const emailLogin = document.querySelector("#emailLogin");
+  const passwordLogin = document.querySelector("#passwordLogin");
+  const btnLogin = document.querySelector("#btnLogin");
+
+  formLogin.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    alert("Vous êtes connecté");
+    const formData = {
+      email: emailLogin.value,
+      password: passwordLogin.value,
+    };
+    const email = document.getElementById("emailLogin").value;
+    const password = document.getElementById("passwordLogin").value;
+    console.log(formData);
+    console.log(email);
+    console.log(password);
+    /*
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // set token in local storage
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        alert("Vous êtes connecté");
+        formLogin.reset();
+      })
+      .catch((err) => console.log(err));
+  });*/
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        console.log("ok");
+        localStorage.setItem("token", data.token);
+      } else {
+        console.log("not ok");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+// Envoyer les donnés du formulaire de register au serveur
+document.addEventListener("DOMContentLoaded", () => {
+  const formRegister = document.querySelector("#RegisterForm");
+  const usernameRegister = document.querySelector("#usernameRegister");
+  const emailRegister = document.querySelector("#emailRegister");
+  const passwordRegister = document.querySelector("#passwordRegister");
+  const btnRegister = document.querySelector("#btnRegister");
+
+  formRegister.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    alert("Votre compte est crée");
+    const formData = {
+      username: usernameRegister.value,
+      email: emailRegister.value,
+      password: passwordRegister.value,
+    };
+    console.log(formData);
+
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        console.log("ok");
+      } else {
+        console.log("not ok");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+// Send to server contact form
+document.addEventListener("DOMContentLoaded", () => {
+  const formRegister = document.querySelector("#RegisterForm");
+  const usernameRegister = document.querySelector("#usernameRegister");
+  const emailRegister = document.querySelector("#emailRegister");
+  const passwordRegister = document.querySelector("#passwordRegister");
+  const btnRegister = document.querySelector("#btnRegister");
+
+  formRegister.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    alert("Votre compte est crée");
+    const formData = {
+      username: usernameRegister.value,
+      email: emailRegister.value,
+      password: passwordRegister.value,
+    };
+    console.log(formData);
+
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        console.log("ok");
+      } else {
+        console.log("not ok");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+// Deconnexion
+
+const logoutButton = document.getElementById("logoutButton");
+logoutButton.addEventListener("click", async function () {
+  const token = localStorage.getItem("accessToken");
+  console.log(token);
+  try {
+    const response = await fetch("https://localhost:3000/api/auth/logout", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("data", data);
+      localStorage.removeItem("accessToken"); // Supprimer l'accessToken du localStorage
+      localStorage.removeItem("loggedIn"); // Réinitialiser le statut de connexion
+      window.location.href = "index.html"; // Rediriger vers la page de connexion
+    } else {
+      console.log("La demande de déconnexion a échoué.");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("erreur");
+  }
+});
